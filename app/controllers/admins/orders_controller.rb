cc3327bec7ee8f7b002f1206f6b58672e4802dee
RyoUchida0415 <tkkndshr2565@gmail.com>
2020-08-17 22:10:37 +0900

    オーダーとプロダクトのコントローラ追加

---
+++ app/controllers/admins/orders_controller.rb
@@ -0,0 +1,19 @@
+class Admin::OrdersController < ApplicationController
+	def index
+		@orders = Order.all
+	end
+
+	def show
+		@order = Order.find(params[:id])
+	end
+
+	def update
+		@order = Order.find(params[:id])
+		@order.update(order_params)
+		redirect_to admin_order_path(order.id)
+	end
+
+	private
+	def order_params
+		params.require(:order).permit(:name,:shipping_fee,:postal_code,:adress,:payment_methods,:billing_amount,:order_status)
+	end
---
+++ app/controllers/admins/products_controller.rb
@@ -0,0 +1,35 @@
+class Admin::ProductsController < ApplicationController
+	def index
+		@products = Product.all
+	end
+
+	def new
+		@product = Product.new
+	end
+
+	def show
+		@product = Product.find(params[:id])
+	end
+
+	def create
+		@product = Product.new(product_params)
+		@product.save
+		redirect_to admin_product_path(product.id)
+	end
+
+	def edit
+		@product = Product.find(params[:id])
+	end
+
+	def update
+		@product = Product.find(params[:id])
+		@product.update(product_params)
+		redirect_to admin_product_path(product.id)
+	end
+
+	private
+	def product_params
+		params.require(:product).permit(:name,:price,:is_active,:production_introduction,:image_id)
+	end
+
+end
