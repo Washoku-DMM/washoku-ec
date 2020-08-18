6f10c7a1b3f36c0c5eff33e1fbccfe2713c06171
vagrant <vagrant@localhost.localdomain>
2020-08-18 06:55:21 +0000

    controllercreate

--- app/controllers/users/cart_products_controller.rb
+++ app/controllers/users/cart_products_controller.rb
@@ -1,2 +1,49 @@
 class Users::CartProductsController < ApplicationController
+
+def index
+	@carts = Cart_product.all
+
+end
+
+def create
+	@cart = Cart_product.new
+	@carts = Cart_product.all
+
+end
+
+def edit
+	@cart = Cart_product.find(params[:id])
+end
+
+def update
+	@cart = Cart_product.find(params[:id])
+	@cart.update
+end
+
+def destroy
+	@cart = Cart_product.find(params[:id])
+	cart.destroy
+	render "index"
+
+	@carts = Cart_products.all
+	carts.destroy
+	render "index"
+
+end
+
+def product_params
+    # params.require(:product).permit(:name, :price)
+    # 税込価格の表示方法
+end
+
+def cart_params
+    params.require(:cart_product).permit(:count)
+end
+
+
+
+
+
+
+
 end
