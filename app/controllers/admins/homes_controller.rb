66479e3410058e093a612198b43971e278fb8bf1
breastroke423 <66353006+breastroke423@users.noreply.github.com>
2020-08-19 20:27:03 +0900

    Update homes_controller.rb
    コンフリクト修正

--- app/controllers/admins/homes_controller.rb
+++ app/controllers/admins/homes_controller.rb
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-class Admin::HomesController < ApplicationController
-=======
 class Admins::HomesController < ApplicationController
->>>>>>> b0b12f333e0bd97cbdaf9a76cfdf18fc79d76657
 end
