efe46772ab1c5ed206b282301b9bd319d0679ca8
breastroke423 <66353006+breastroke423@users.noreply.github.com>
2020-08-19 20:33:48 +0900

    Update genres_controller.rb
    コンフリクト修正

--- app/controllers/users/genres_controller.rb
+++ app/controllers/users/genres_controller.rb
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-class User::GenresController < ApplicationController
-=======
 class Users::GenresController < ApplicationController
->>>>>>> b0b12f333e0bd97cbdaf9a76cfdf18fc79d76657
 end
