1d58b3206659e21ac27c6fb754d6a63cf4802b85
RyoUchida0415 <tkkndshr2565@gmail.com>
2020-08-19 17:27:50 +0900

    add view

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -2,15 +2,23 @@ class Admins::OrdersController < ApplicationController
 
   def index
     @orders = Order.all
+    @custmer =
+    @order_product =
   end
+
   def show
     @order = Order.find(params[:id])
+    @custmer = Customer.find(params[:id])
+    @delivery = Delivery.find(params[:id])
+    @product = Product.find(params[:id])
   end
+
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
     redirect_to admin_order_path(order.id)
   end
+
   private
   def order_params
     params.require(:order).permit(:name,:shipping_fee,:postal_code,:adress,:payment_methods,:billing_amount,:order_status)
