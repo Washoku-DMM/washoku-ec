18bd15d7771dcf850f97e8c2769d3133a6d6612d
vagrant <vagrant@localhost.localdomain>
2020-08-19 09:04:21 +0000

    cutomer/orderviewcreate

--- app/controllers/users/orders_controller.rb
+++ app/controllers/users/orders_controller.rb
@@ -1,55 +1,47 @@
-<<<<<<< HEAD
 class User::OrdersController < ApplicationController
 
    def index
-   	   @orders = Order.all
+   	   @orders = Order.all(@user)
    	   @user = current_user
    end
 
-
    def show
    	   @order = Order.find(params[:id])
-   	   
+   end
+
+   def new
+   		@order = Order.new
+   end
+
+   def confirm
+   		@cart = Cart.all
+   		@order = Order.new(order_params)
+   		render :new if @order.invalid?
    end
 
    def create
+	   	@order = Order.new(order_params)
+	   	respond_to do |format|
+	   		if params[:back]
+	   			format.html { render :new }
+	   		elsif @order.save
+	   			format.html { redirect_to @,}
+	   			format.json { render :show, status: :created, location: @order }
+	   		else
+	   			format.html { render :new }
+	   			format.json { render json: @order.errors, status: :unprocessable_entity }
+	   		end
+	   	end
    end
 
-   def update
+   def about
    end
-=======
-class Users::OrdersController < ApplicationController
 
-# orderコントローラー話し合ってからかく
+   private
 
-
-def index
-	@user = current_user
-	@orders = Order.all(@user)
-end
-
-def show
-	@order = Order.find(params[id])
-
-end
-
-def create
-	@order = Order.new
-
-end
-
-def update ?
-	@order =Order.find(params[id])
-end
-
-def about
-end
-
-
-
-
-
->>>>>>> b0b12f333e0bd97cbdaf9a76cfdf18fc79d76657
+   def order_params
+    params.require(:order).permit(:shipping_fee, :postal_code, :address, :name, :postal_code, :adress, :payment_methods, :billing_amount, :order_status)
+   end
 
 
 end
