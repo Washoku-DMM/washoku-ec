f86e119f9788f1967e549e2342cab393b460c0c2
nitta-hayatogit <06hayato18@gmail.com>
2020-08-23 13:48:12 +0900

    商品一覧のview,controllerの修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,14 +2,25 @@
 class ProductsController < ApplicationController
 
 def index
-	# ジャンル選択されているなら
-	if
-	@products = Product.find(prpduct.genre.id).paginate(page: params[:page], per_page: 8)
-	#↑ジャンルidから持ってこないといけない
+	# # ジャンル選択されているなら
+	# if
+	# @products = Product.find(prpduct.genre.id).paginate(page: params[:page], per_page: 8)
+	# #↑ジャンルidから持ってこないといけない
 
-    else   #それ以外（ジャンル選択されていないなら）
-	@products = Product.all.paginate(page: params[:page], per_page: 8)
-	
+ #    else   #それ以外（ジャンル選択されていないなら）
+	# @products = Product.all.paginate(page: params[:page], per_page: 8)
+
+
+# 　　　　8/22
+    sort = params[:sort]
+   product_genres = nil
+ if product_genres
+	 @products = Product.where(product_genre_id: product_genres).
+	                     paginate(page: params[:page], per_page: 8)
+     else
+	 @products = Product.all.paginate(page: params[:page], per_page: 8)
+	 end
+	# お手本コード参照バージョン
 
 
 
@@ -20,7 +31,7 @@ def index
 
 
 end
-
+end
 def show
 	@product =Product.find(params[:id])
 	@user = User.find(pramas[:id])
@@ -32,5 +43,3 @@ end
 # 	rende '/cart_products'
 # end
 # あとで話し合う
-
-end
