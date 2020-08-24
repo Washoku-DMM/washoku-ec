568b7206e9929900f032217f07d4dbc3938a24e6
nitta-hayato <06hayato18@gmail.com>
2020-08-24 12:28:54 +0000

    おすすめ商品追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,8 +3,10 @@ class HomesController < ApplicationController
 
 def top
 	@genres = Genre.all
-	@rank_products = OrderProduct.find(OrderProduct.group(:count).order('count(count) desc').limit(4).pluck(:order_product_id))
+	@rank_products = Product.find(OrderProduct.group(:product_id).order('count(product_id) desc').limit(4).pluck(:product_id))
+	
 end
+
 
 
 def about
