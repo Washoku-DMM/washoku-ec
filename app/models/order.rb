732ea43d14554adc652aa4c9af45d97859c6a104
breastroke423 <66353006+breastroke423@users.noreply.github.com>
2020-08-26 20:24:26 +0900

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,10 +2,4 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_products, dependent: :destroy
   has_many :products, :through => :order_products
-
-# if order_status == 1 then
-#   order_products.product_status == 1
-# end
-
-
 end
