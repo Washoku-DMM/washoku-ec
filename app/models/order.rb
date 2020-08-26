19af7887c84db94318b6b9d4e7ade6df38f3b7fb
vagrant <vagrant@localhost.localdomain>
2020-08-26 11:19:59 +0000

    add admin/order/view

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,9 @@ class Order < ApplicationRecord
   has_many :order_products, dependent: :destroy
   has_many :products, :through => :order_products
 
+# if order_status == 1 then
+#   order_products.product_status == 1
+# end
+
 
 end
--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -1,4 +1,9 @@
 class OrderProduct < ApplicationRecord
 	belongs_to :order
 	belongs_to :product
+
+# if product.product_status == 3 then
+#   orders.order_status == 3
+# end
+
 end
