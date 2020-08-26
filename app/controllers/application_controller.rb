efd81a62a1a5e2c785b2a6eba20f2e8fbba9b531
vagrant <vagrant@localhost.localdomain>
2020-08-26 03:52:18 +0000

    utc->jst

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+
   	def after_sign_in_path_for(resource)
 		case resource
 
@@ -10,21 +11,18 @@ class ApplicationController < ActionController::Base
 	end
 
   def after_sign_out_path_for(resource)
+
     if Admin
       new_admins_admin_session_path
-    else Customer
+    if Customer
       new_customers_customer_session_path
     end
   end
+end
 
-
-
-  #カラム追加
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :address, :phone_number])
   end
-
 end
